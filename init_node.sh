a2a492e0ae7e768f028f4389fe26700c7a510108
Hugh Brien <hugh@hughbrien.com>
2014-12-13 12:05:01 -0500

    updated book download

--- init_node.sh
+++ init_node.sh
@@ -12,7 +12,7 @@ export HADOOP_OPTS=-Djava.net.preferIPv4Stack=true
 
 /opt/hadoop/hadoop/bin/hadoop namenode -format
 cd / 
-wget http://www.gutenberg.org/ebooks/25665.epub.noimages?session_id=92c8b92a1db8f09df509d3a32ba744f545bfbf27
+wget -o book.txt  http://www.gutenberg.org/ebooks/25665.epub.noimages?session_id=92c8b92a1db8f09df509d3a32ba744f545bfbf27
 /opt/hadoop/hadoop/bin/hadoop fs -mkdir /wordcount-in
 /opt/hadoop/hadoop/bin/hadoop fs -mkdir /wordcount-out
 cd  /opt/hadoop/hadoop
